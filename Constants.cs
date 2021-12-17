106fd564d6a144bdcba68e045faac75bc34ae179
kagnous <antoinecasa34@gmail.com>
2021-12-17 10:30:11 +0100

    AJout barre de vie + Constant

---
+++ Constants.cs
@@ -0,0 +1,13 @@
+namespace KagnousLib
+{
+    /// <summary>
+    /// Classe qui contient tout les noms des noms/liens universels<br/>(comme ça on a qu'un endroit à modifier)
+    /// </summary>
+    public static class Constants
+    {
+        public const string BASE_HELP_URL = "https://github.com/kagnous/Librarie_Unity.git";
+        public const string DEFAULT_HELP_URL  = BASE_HELP_URL + "/README.md";
+
+        public const string LIB = "_My Library";
+    }
+}
